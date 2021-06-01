70705ab0fc1fc48254ab1608d2d8d88b605dd2bc
thiagorjes <thiago_rj_es@hotmail.com>
2021-05-31 21:50:36 -0300

    corrigido o script de instalação das dependenciar

--- instalar_dependencias.sh
+++ instalar_dependencias.sh
@@ -3,7 +3,7 @@
 CUDA_10_INSTALL=$1
 
 # precisa do pip/python para funcionar - é python3
-if [ $CUDA_10_INSTALL ]; then
+if  [ "$CUDA_10_INSTALL" == 1  ]; then
     echo "installing cuda 10 (depends)..."
     sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
     sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
